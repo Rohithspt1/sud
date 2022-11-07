22088a2525927547195ddc0499650d2c9c0adb14
Rohithspt1 <rohithrao444@gmail.com>
2022-11-06 23:09:34 -0800

    change

---
+++ ns.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  6 22:52:57 2022
+
+@author: SPTINT-23
+"""
+
+l1=[1,2,3,4,56,7]
+print(l1)
+print(l1[2])
+print(l1[3])
+print(l1[0:1])
+print(l1[2:5])
